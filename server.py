96412449d78cc6bdeb4994d93a33c8907a646213
pshriwise <shriwise@wisc.edu>
2015-02-02 11:21:08 -0600

    Wrapped standard request handler in a new one and redefined the do_GET function.

--- server.py
+++ server.py
@@ -2,9 +2,15 @@
 
 import SimpleHTTPServer, SocketServer
 
-handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+class Handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)     #  Send 200 OK
+        self.end_headers()
+        self.wfile.write("Hello Bucky!")
+
 
 PORT=4000
-httpd = SocketServer.ThreadingTCPServer(("", PORT), handler ) # Can also use ForkingTCPServer
+httpd = SocketServer.ThreadingTCPServer(("", PORT), Handler) # Can also use ForkingTCPServer
 print "serving at port", PORT
 httpd.serve_forever()
