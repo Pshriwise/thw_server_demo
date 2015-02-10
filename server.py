6ac811643f13db8a1de58b52fdae543b57b7437c
pshriwise <shriwise@wisc.edu>
2015-02-10 12:38:37 -0600

    Removed post functionality. Added info to 'readme'.

--- server.py
+++ server.py
@@ -13,9 +13,6 @@ class Handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
     def do_GET(self):
         
-        h5ms = os.listdir("./")
-        h5ms = [x for x in h5ms if ".h5m" in x]
-        if len(h5ms) == 0: h5ms = ["None"]
 
         if "/Slice" in self.path:
             
@@ -30,8 +27,7 @@ class Handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
             if len(args) > 1: 
                 args = args[1]
             else:
-                self.wfile.write("Please enter a valid filename:\n")
-                for x in h5ms: self.wfile.write(x+"\n")
+                self.readme()
                 return
             args = args.split('&')
 
@@ -41,8 +37,9 @@ class Handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
                 arg_dict[temp[0]]=temp[1]
 
             #create the slice plot
-            if not arg_dict.has_key('filename'):
-                self.wfile.write("Please enter a valid filename")
+            if 'filename' not in arg_dict or 'axis' not in arg_dict or 'coord' not in arg_dict :
+                self.readme()
+                return
             filename = arg_dict['filename']
             axis = arg_dict['axis']
             coord = arg_dict ['coord']
@@ -54,45 +51,24 @@ class Handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
             self.wfile.write( f.read() )
             return
 
-        if "/Upload" in self.path:
-            self.send_response(200)
-            self.send_header("Content-type","text/html")
-            self.wfile.write("""\<!DOCTYPE html>
-<html>
-<body>
-
-<form action="Slice" method="post" enctype="multipart/form-data">
-    Select image to upload:
-    <input type="file" name="fileToUpload" id="fileToUpload">
-    <input type="submit" value="Upload .h5m File" name="submit">
-</form>
-
-</body>
-                             </html>""")
-            return
+        
+        self.wfile.write("Path is invalid.")
         self.send_response(200)
-        self.wfile.write("URL is invalid.")
+        self.end_headers()
         return 
 
-    def do_POST(self):
-        # We handle here all the POSTs
-        self.send_response(200)
-        form = cgi.FieldStorage(
-            fp=self.rfile,
-            headers=self.headers,
-            environ={'REQUEST_METHOD':'POST',
-                     'CONTENT_TYPE':self.headers['Content-Type'],
-                     })
-        fileitem = form['fileToUpload']
-        filename = fileitem.filename
-        fileout = open(filename, 'wb')
-        while 1:
-            chunk = fileitem.file.read(10000)
-            if not chunk : break 
-            fileout.write(chunk)
-        fileout.close()
         
-
+    def readme(self):
+        h5ms = os.listdir("./")
+        h5ms = [x for x in h5ms if ".h5m" in x]
+        if len(h5ms) == 0: h5ms = ["None"]
+        
+        self.wfile.write("Please enter a valid filename:\n")
+        for x in h5ms: self.wfile.write(x+"\n")
+        self.wfile.write("\n path format is /Slice?filename=<your_filename>&axis=<slice_axis>&coord=<slice_coordinate> \n")
+        self.wfile.write("\n NOTE: slices only garaunteed for watertight files.")
+        return
+    
 
 PORT=4000
 httpd = SocketServer.ThreadingTCPServer(("", PORT), Handler) # Can also use ForkingTCPServer
